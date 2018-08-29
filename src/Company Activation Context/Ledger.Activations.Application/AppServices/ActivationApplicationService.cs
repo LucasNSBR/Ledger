f26ceac53124a048934339cb2707d4ebe3a0a032
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-08-29 01:30:51 -0300

    Add application services structure

---
+++ src/Company Activation Context/Ledger.Activations.Application/AppServices/ActivationApplicationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ledger.Activations.Domain.Aggregates.ActivationAggregate;
+
+namespace Ledger.Activations.Application.AppServices
+{
+    public class ActivationApplicationService : IActivationApplicationService
+    {
+        public Activation GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Activation GetByCompanyId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Company Activation Context/Ledger.Activations.Application/AppServices/IActivationApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using Ledger.Activations.Domain.Aggregates.ActivationAggregate;
+using System;
+
+namespace Ledger.Activations.Application.AppServices
+{
+    public interface IActivationApplicationService
+    {
+        Activation GetById(Guid id);
+        Activation GetByCompanyId(Guid id);
+    }
+}
