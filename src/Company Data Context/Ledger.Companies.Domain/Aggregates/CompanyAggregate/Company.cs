f2d7e446ff1603e08a1a7ccdb243f63d977398a5
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-08-28 22:17:05 -0300

    Add company aggregate

---
+++ src/Company Data Context/Ledger.Companies.Domain/Aggregates/CompanyAggregate/Company.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ledger.Companies.Domain.Aggregates.CompanyAggregate
+{
+    public class Company
+    {
+        public Guid Id { get; private set; }
+
+        public bool Active { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public string Cnpj { get; private set; }
+
+        public Company(string name, string description, string cnpj)
+        {
+            Active = false;
+
+            Name = name;
+            Description = description;
+            Cnpj = cnpj;
+        }
+
+        public Company(Guid id, string name, string description, string cnpj)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Cnpj = cnpj;
+        }
+    }
+}
