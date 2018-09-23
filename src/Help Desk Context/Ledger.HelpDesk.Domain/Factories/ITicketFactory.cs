b683e92d2b89ca6590157a3c639f98cd7d65273c
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-09-23 00:20:41 -0300

    Add Ticket factories

---
+++ src/Help Desk Context/Ledger.HelpDesk.Domain/Factories/ITicketFactory.cs
@@ -0,0 +1,11 @@
+﻿using Ledger.HelpDesk.Domain.Aggregates.CategoryAggregate;
+using Ledger.HelpDesk.Domain.Aggregates.TicketAggregate;
+using System;
+
+namespace Ledger.HelpDesk.Domain.Factories
+{
+    public interface ITicketFactory
+    {
+        Ticket Open(Guid id, string title, string details, TicketCategory category);
+    }
+}
---
+++ src/Help Desk Context/Ledger.HelpDesk.Domain/Factories/TicketFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ledger.HelpDesk.Domain.Aggregates.CategoryAggregate;
+using Ledger.HelpDesk.Domain.Aggregates.TicketAggregate;
+
+namespace Ledger.HelpDesk.Domain.Factories
+{
+    public class TicketFactory : ITicketFactory
+    {
+        public Ticket Open(Guid id, string title, string details, TicketCategory category)
+        {
+            return new Ticket(id, title, details, category);
+        }
+    }
+}
