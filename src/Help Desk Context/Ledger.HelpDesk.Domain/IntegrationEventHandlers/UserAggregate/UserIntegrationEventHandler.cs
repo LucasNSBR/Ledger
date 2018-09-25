f593163bda1824bdf7ace7f09da27a533cd13286
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-09-24 21:39:39 -0300

    Add UserIntegrationEventHandler structure

---
+++ src/Help Desk Context/Ledger.HelpDesk.Domain/IntegrationEventHandlers/UserAggregate/UserIntegrationEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using Ledger.Shared.IntegrationEvents.Events.UserEvents;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace Ledger.HelpDesk.Domain.IntegrationEventHandlers.UserAggregate
+{
+    public class UserIntegrationEventHandler : IConsumer<UserRegisteredIntegrationEvent>,
+                                               IConsumer<UserAddedSupportRoleIntegrationEvent>
+    {
+        public Task Consume(ConsumeContext<UserRegisteredIntegrationEvent> context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Consume(ConsumeContext<UserAddedSupportRoleIntegrationEvent> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
