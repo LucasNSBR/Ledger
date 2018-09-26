abed2ddd4fca22480312bd35a628c4009621e349
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-09-26 01:15:33 -0300

    Add Role application services & commands

---
+++ src/Help Desk Context/Ledger.HelpDesk.Application/AppServices/RoleAppServices/IRoleApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using Ledger.HelpDesk.Domain.Aggregates.RoleAggregate;
+using System;
+
+namespace Ledger.HelpDesk.Application.AppServices.RoleAppServices
+{
+    public interface IRoleApplicationService
+    {
+        Role GetById(Guid id);
+        Role GetByName(string name);
+        void Register(RegisterRoleCommand command);
+        void Remove(RemoveRoleCommand command);
+    }
+}
---
+++ src/Help Desk Context/Ledger.HelpDesk.Domain/Commands/RoleCommands/RegisterRoleCommand.cs
@@ -0,0 +1,21 @@
+﻿using Ledger.Shared.Commands;
+using Ledger.Shared.Extensions;
+using LilValidation.Core;
+
+namespace Ledger.HelpDesk.Domain.Commands.RoleCommands
+{
+    public class RegisterRoleCommand : Command
+    {
+        public string Name { get; set; }
+
+        public override void Validate()
+        {
+            new ValidationContract<RegisterRoleCommand, string>(this, command => command.Name)
+                .NotEmpty()
+                .MinLength(3)
+                .MaxLength(50)
+                .Build()
+                .AddToNotifier(this);
+        }
+    }
+}
