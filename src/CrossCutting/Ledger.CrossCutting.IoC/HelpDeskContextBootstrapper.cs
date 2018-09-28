28d82fbccc7309e56fc6b0e5331377f68b4b06f2
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-09-28 00:30:14 -0300

    Add Ticket dependencies to IoC container

--- src/CrossCutting/Ledger.CrossCutting.IoC/HelpDeskContextBootstrapper.cs
+++ src/CrossCutting/Ledger.CrossCutting.IoC/HelpDeskContextBootstrapper.cs
@@ -1,7 +1,12 @@
-﻿using Ledger.HelpDesk.Application.AppServices.TicketCategoryAppServices;
+﻿using Ledger.HelpDesk.Application.AppServices.TicketAppServices;
+using Ledger.HelpDesk.Application.AppServices.TicketCategoryAppServices;
 using Ledger.HelpDesk.Data.Context;
+using Ledger.HelpDesk.Data.Repositories.RoleRepositories;
 using Ledger.HelpDesk.Data.Repositories.TicketCategoryRepositories;
+using Ledger.HelpDesk.Data.Repositories.TicketRepositories;
+using Ledger.HelpDesk.Domain.Repositories.RoleRepositories;
 using Ledger.HelpDesk.Domain.Repositories.TicketCategoryRepositories;
+using Ledger.HelpDesk.Domain.Repositories.TicketRepositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,11 +25,14 @@ namespace Ledger.CrossCutting.IoC
         
         private static void InitializeRepositories(IServiceCollection services)
         {
+            services.AddScoped<ITicketRepository, TicketRepository>();
             services.AddScoped<ITicketCategoryRepository, TicketCategoryRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
         }
         
         private static void InitializeApplicationServices(IServiceCollection services)
         {
+            services.AddScoped<ITicketApplicationService, TicketApplicationService>();
             services.AddScoped<ITicketCategoryApplicationService, TicketCategoryApplicationService>();
         }
     }
