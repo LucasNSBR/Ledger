73bbd188629d4d90a9ac7d0da6eaa9c1a534aeda
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-02 01:55:56 -0300

    Add City/State repositories

---
+++ Ledger.Shared.Locations/Repositories/StateRepositories/IStateRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ledger.Shared.Entities;
+using System;
+using System.Linq;
+
+namespace Ledger.Shared.Locations.Repositories.StateRepositories
+{
+    public interface IStateRepository
+    {
+        IQueryable<State> GetAllStates();
+        State GetById(Guid id);
+    }
+}
---
+++ Ledger.Shared.Locations/Repositories/StateRepositories/StateRepository.cs
@@ -0,0 +1,36 @@
+﻿using Ledger.Shared.Entities;
+using Ledger.Shared.Locations.Context;
+using Ledger.Shared.Locations.Specifications.StateSpecifications;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Ledger.Shared.Locations.Repositories.StateRepositories
+{
+    public class StateRepository : IStateRepository
+    {
+        private readonly LedgerLocationDbContext _dbContext;
+        private readonly DbSet<State> _dbSet;
+
+        public StateRepository(LedgerLocationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.States;
+        }
+
+        public IQueryable<State> GetAllStates()
+        {
+            return _dbSet
+                .AsNoTracking();
+        }
+
+        public State GetById(Guid id)
+        {
+            StateIdSpecification specification = new StateIdSpecification(id);
+
+            return _dbSet
+                .AsNoTracking()
+                .FirstOrDefault(specification.ToExpression());
+        }
+    }
+}
