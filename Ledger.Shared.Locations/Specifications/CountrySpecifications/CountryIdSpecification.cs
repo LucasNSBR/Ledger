369e38d52c53ac50b09ebcd70c2f119f564bc969
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-02 20:13:16 -0300

    Add Country specifications

---
+++ Ledger.Shared.Locations/Specifications/CountrySpecifications/CountryIdSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Ledger.Shared.Entities.Locations;
+using Ledger.Shared.Specifications;
+using System;
+using System.Linq.Expressions;
+
+namespace Ledger.Shared.Locations.Specifications.CountrySpecifications
+{
+    public class CountryIdSpecification : BaseSpecification<Country>
+    {
+        private readonly Guid _id;
+
+        protected CountryIdSpecification(Guid id)
+        {
+            _id = id;
+        }
+
+        public override Expression<Func<Country, bool>> ToExpression()
+        {
+            return c => c.Id == _id;
+        }
+    }
+}
