f2fd7a4fd8032078168562230927ebfe56ab00c1
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-05 21:03:29 -0300

    Add Storage service dependencies to IoC container

---
+++ src/CrossCutting/Ledger.CrossCutting.IoC/StorageServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Ledger.CrossCutting.Storage.Configuration;
+using Ledger.CrossCutting.Storage.Service;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Ledger.CrossCutting.IoC
+{
+    public static partial class DependencyInjectionExtensions
+    {
+        /// <summary>
+        /// Add Dependencies for Storage Service
+        /// </summary>
+        /// <param name="services">List of services to register</param>
+        /// <param name="setupAction">List of configurations to Azure Blob Storage</param>
+        /// <returns></returns>
+        public static IServiceCollection AddEmailService(this IServiceCollection services, Action<StorageOptions> setupAction)
+        {
+            services.Configure(setupAction);
+
+            services.AddScoped<IStorageService, StorageService>();
+
+            return services;
+        }
+    }
+}
