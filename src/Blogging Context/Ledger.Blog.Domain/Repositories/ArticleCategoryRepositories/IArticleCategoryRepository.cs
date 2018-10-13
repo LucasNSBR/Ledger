fa005bed72ee0ea0ed6ff3dbf741c153e0bf896f
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-13 20:01:32 -0300

    Add Article/Category repositories

---
+++ src/Blogging Context/Ledger.Blog.Domain/Repositories/ArticleCategoryRepositories/IArticleCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Ledger.Blog.Domain.Aggregates.CategoryAggregate;
+using System;
+using System.Linq;
+
+namespace Ledger.Blog.Domain.Repositories.ArticleCategoryRepositories
+{
+    public interface IArticleCategoryRepository
+    {
+        IQueryable<ArticleCategory> GetAllCategories();
+        ArticleCategory GetById(Guid id);
+        void Register(ArticleCategory articleCategory);
+        void Update(ArticleCategory articleCategory);
+        void Remove(ArticleCategory articleCategory);
+    }
+}
---
+++ src/Blogging Context/Ledger.Blog.Domain/Repositories/ArticleRepositories/IArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Ledger.Blog.Domain.Aggregates.ArticleAggregate;
+using System;
+using System.Linq;
+
+namespace Ledger.Blog.Domain.Repositories.ArticleRepositories
+{
+    public interface IArticleRepository
+    {
+        IQueryable<Article> GetAllArticles();
+        IQueryable<Article> GetByCategory(Guid categoryId);
+        Article GetById(Guid id);
+        void Register(Article article);
+        void Update(Article article);
+        void Remove(Article article);
+    }
+}
