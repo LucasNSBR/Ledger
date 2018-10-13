c5141183d5c0b4cbbfba6bdeb4ea2788fa6c807c
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-13 20:05:50 -0300

    Add Article/Category specifications

---
+++ src/Blogging Context/Ledger.Blog.Domain/Specifications/ArticleCategorySpecifications/ArticleCategoryIdSpecification.cs
@@ -0,0 +1,22 @@
+﻿using Ledger.Blog.Domain.Aggregates.CategoryAggregate;
+using Ledger.Shared.Specifications;
+using System;
+using System.Linq.Expressions;
+
+namespace Ledger.Blog.Domain.Specifications.ArticleCategorySpecifications
+{
+    public class ArticleCategoryIdSpecification : BaseSpecification<ArticleCategory>
+    {
+        private readonly Guid _id;
+
+        public ArticleCategoryIdSpecification(Guid id)
+        {
+            _id = id;
+        }
+
+        public override Expression<Func<ArticleCategory, bool>> ToExpression()
+        {
+            return ac => ac.Id == _id;
+        }
+    }
+}
