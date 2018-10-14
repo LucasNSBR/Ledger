4b51dfa74730a93ab12df5a72ddf1803d058bce2
Lucas Pereira Campos <lucas.pereira.campos@hotmail.com>
2018-10-14 01:11:07 -0300

    Add EntityTypeConfiguration for Comment

---
+++ src/Blogging Context/Ledger.Blog.Data/Ledger.Blog.Data/EntityTypeConfiguration/ArticleTypeConfiguration/CommentEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Ledger.Blog.Domain.Aggregates.ArticleAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ledger.Blog.Data.EntityTypeConfiguration.ArticleTypeConfiguration
+{
+    public class CommentEntityTypeConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder
+                .HasKey(k => k.Id);
+
+            builder
+                .Property(c => c.Body)
+                .IsRequired()
+                .HasMaxLength(250);
+
+            builder
+                .Property(c => c.AuthorId)
+                .IsRequired();
+        }
+    }
+}
